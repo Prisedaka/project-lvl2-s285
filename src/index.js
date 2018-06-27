a097ed4ca5ebc360fd42f1369240490cb3d3ed59
Prisedaka <prisedaka@ya.ru>
2018-06-27 19:54:08 +0300

    public s2i2

--- src/index.js
+++ src/index.js
@@ -4,19 +4,16 @@ import fs from 'fs';
 const gendiff = (path1, path2) => {
   const obj1 = JSON.parse(fs.readFileSync(path1, 'utf8'));
   const obj2 = JSON.parse(fs.readFileSync(path2, 'utf8'));
-  const diffObj1 = Object.keys(obj1).reduce((acc, elem) => {
+  const unionArrKey = _.union(Object.keys(obj1), Object.keys(obj2));
+  const diff = unionArrKey.map((elem) => {
     let newElem = '';
-    if (_.has(obj2, elem)) {
+    if (_.has(obj2, elem) && _.has(obj1, elem)) {
       if (obj1[elem] === obj2[elem]) newElem = `    ${elem}: ${obj1[elem]}\n`;
       else newElem = `  + ${elem}: ${obj2[elem]}\n  - ${elem}: ${obj1[elem]}\n`;
-    } else newElem = `  - ${elem}: ${obj1[elem]}\n`;
-    return `${acc}${newElem}`;
-  }, '');
-  const diffObj2 = Object.keys(obj2).reduce((acc, elem) => {
-    let newElem = '';
-    if (!_.has(obj1, elem)) newElem = `  + ${elem}: ${obj2[elem]}\n`;
-    return `${acc}${newElem}`;
-  }, '');
-  return `{\n${diffObj1}${diffObj2}}`;
+    } else if (_.has(obj1, elem)) newElem = `  - ${elem}: ${obj1[elem]}\n`;
+    else if (_.has(obj2, elem)) newElem = `  + ${elem}: ${obj2[elem]}\n`;
+    return newElem;
+  }).join('');
+  return `{\n${diff}}`;
 };
 export default gendiff;
