ade23760f9841f4c775ed7b7511319eed4733348
Prisedaka <prisedaka@ya.ru>
2018-06-27 17:24:38 +0300

    correct

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,12 +1,11 @@
 import fs from 'fs';
 import genDiff from '../src/';
 
-const pathToFile1 = './__tests__/__fixtures__/before.json';
-const pathToFile2 = './__tests__/__fixtures__/after.json';
-const pathToResult = './__tests__/__fixtures__/result.json';
-
-const diff = genDiff(pathToFile1, pathToFile2);
-const expected = fs.readFileSync(pathToResult, 'utf8');
 test('differences', () => {
+  const pathToFile1 = './__tests__/__fixtures__/before.json';
+  const pathToFile2 = './__tests__/__fixtures__/after.json';
+  const pathToResult = './__tests__/__fixtures__/result.json';
+  const expected = fs.readFileSync(pathToResult, 'utf8');
+  const diff = genDiff(pathToFile1, pathToFile2);
   expect(diff).toBe(expected);
 });
--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -6,5 +6,5 @@ commander
   .version('1.0.9')
   .arguments('<firstConfig> <secondConfig>')
   .option('-f, --format [type]', 'output format')
-  .action((firstConfig, secondConfig) => console.log(gendiff(`./${firstConfig}`, `./${secondConfig}`)))
+  .action((firstConfig, secondConfig) => console.log(gendiff(firstConfig, secondConfig)))
   .parse(process.argv);
