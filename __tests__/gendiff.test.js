73f0c9f8aab1c2af7e5009793377442ecf5a8f56
Prisedaka <prisedaka@ya.ru>
2018-06-28 23:23:27 +0300

    -

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -2,26 +2,26 @@ import fs from 'fs';
 import genDiff from '../src/';
 
 test('differences json', () => {
-  const pathToFile1 = './__tests__/__fixtures__/before.json';
-  const pathToFile2 = './__tests__/__fixtures__/after.json';
-  const pathToResult = './__tests__/__fixtures__/result';
+  const pathToFile1 = './__tests__/__fixtures__/beforeAST.json';
+  const pathToFile2 = './__tests__/__fixtures__/afterAST.json';
+  const pathToResult = './__tests__/__fixtures__/resultAST';
   const expected = fs.readFileSync(pathToResult, 'utf8');
   const diff = genDiff(pathToFile1, pathToFile2);
   expect(diff).toBe(expected);
 });
-test('differences yml', () => {
-  const pathToFile1 = './__tests__/__fixtures__/before.yml';
-  const pathToFile2 = './__tests__/__fixtures__/after.yml';
-  const pathToResult = './__tests__/__fixtures__/result';
-  const expected = fs.readFileSync(pathToResult, 'utf8');
-  const diff = genDiff(pathToFile1, pathToFile2);
-  expect(diff).toBe(expected);
-});
-test('differences ini', () => {
-  const pathToFile1 = './__tests__/__fixtures__/before.ini';
-  const pathToFile2 = './__tests__/__fixtures__/after.ini';
-  const pathToResult = './__tests__/__fixtures__/result';
-  const expected = fs.readFileSync(pathToResult, 'utf8');
-  const diff = genDiff(pathToFile1, pathToFile2);
-  expect(diff).toBe(expected);
-});
+// test('differences yml', () => {
+//   const pathToFile1 = './__tests__/__fixtures__/before.yml';
+//   const pathToFile2 = './__tests__/__fixtures__/after.yml';
+//   const pathToResult = './__tests__/__fixtures__/result';
+//   const expected = fs.readFileSync(pathToResult, 'utf8');
+//   const diff = genDiff(pathToFile1, pathToFile2);
+//   expect(diff).toBe(expected);
+// });
+// test('differences ini', () => {
+//   const pathToFile1 = './__tests__/__fixtures__/before.ini';
+//   const pathToFile2 = './__tests__/__fixtures__/after.ini';
+//   const pathToResult = './__tests__/__fixtures__/result';
+//   const expected = fs.readFileSync(pathToResult, 'utf8');
+//   const diff = genDiff(pathToFile1, pathToFile2);
+//   expect(diff).toBe(expected);
+// });
--- src/index.js
+++ src/index.js
@@ -3,6 +3,17 @@ import fs from 'fs';
 import path from 'path';
 import parse from './parsers';
 
+const makeAST = (data) => {
+  const [first, ...rest] = data;
+  const root = {
+    name: first,
+    value: '',
+    children: [],
+  };
+  return rest.reduce((acc, arg) => {
+    
+  }, root);
+};
 const gendiff = (path1, path2) => {
   const format1 = path.extname(path1);
   const format2 = path.extname(path2);
@@ -21,3 +32,22 @@ const gendiff = (path1, path2) => {
   return `{\n${diff}}`;
 };
 export default gendiff;
+
+// const gendiff = (path1, path2) => {
+//   const format1 = path.extname(path1);
+//   const format2 = path.extname(path2);
+//   const data1 = fs.readFileSync(path1, 'utf8');
+//   const data2 = fs.readFileSync(path2, 'utf8');
+//   const obj1 = parse(format1)(data1);
+//   const obj2 = parse(format2)(data2);
+//   const unionArrKey = _.union(Object.keys(obj1), Object.keys(obj2));
+//   const diff = unionArrKey.map((elem) => {
+//     if (_.has(obj2, elem) && _.has(obj1, elem)) {
+//       if (obj1[elem] === obj2[elem]) return `    ${elem}: ${obj1[elem]}\n`;
+//       return `  + ${elem}: ${obj2[elem]}\n  - ${elem}: ${obj1[elem]}\n`;
+//     } else if (_.has(obj1, elem)) return `  - ${elem}: ${obj1[elem]}\n`;
+//     return `  + ${elem}: ${obj2[elem]}\n`;
+//   }).join('');
+//   return `{\n${diff}}`;
+// };
+// export default gendiff;
