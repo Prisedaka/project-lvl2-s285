6e28a35d09ceefb8b66c9da01b549551726fd402
Prisedaka <prisedaka@ya.ru>
2018-06-30 09:58:35 +0300

    complete s5i1

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -9,19 +9,19 @@ test('differences json', () => {
   const diff = genDiff(pathToFile1, pathToFile2);
   expect(diff).toBe(expected);
 });
-// test('differences yml', () => {
-//   const pathToFile1 = './__tests__/__fixtures__/before.yml';
-//   const pathToFile2 = './__tests__/__fixtures__/after.yml';
-//   const pathToResult = './__tests__/__fixtures__/result';
-//   const expected = fs.readFileSync(pathToResult, 'utf8');
-//   const diff = genDiff(pathToFile1, pathToFile2);
-//   expect(diff).toBe(expected);
-// });
-// test('differences ini', () => {
-//   const pathToFile1 = './__tests__/__fixtures__/before.ini';
-//   const pathToFile2 = './__tests__/__fixtures__/after.ini';
-//   const pathToResult = './__tests__/__fixtures__/result';
-//   const expected = fs.readFileSync(pathToResult, 'utf8');
-//   const diff = genDiff(pathToFile1, pathToFile2);
-//   expect(diff).toBe(expected);
-// });
+test('differences yml', () => {
+  const pathToFile1 = './__tests__/__fixtures__/beforeAST.yml';
+  const pathToFile2 = './__tests__/__fixtures__/afterAST.yml';
+  const pathToResult = './__tests__/__fixtures__/resultAST';
+  const expected = fs.readFileSync(pathToResult, 'utf8');
+  const diff = genDiff(pathToFile1, pathToFile2);
+  expect(diff).toBe(expected);
+});
+test('differences ini', () => {
+  const pathToFile1 = './__tests__/__fixtures__/beforeAST.ini';
+  const pathToFile2 = './__tests__/__fixtures__/afterAST.ini';
+  const pathToResult = './__tests__/__fixtures__/resultAST';
+  const expected = fs.readFileSync(pathToResult, 'utf8');
+  const diff = genDiff(pathToFile1, pathToFile2);
+  expect(diff).toBe(expected);
+});
--- src/index.js
+++ src/index.js
@@ -37,21 +37,27 @@ const makeAst = (firstConfig = {}, secondConfig = {}) => {
   const configsKeys = _.union(Object.keys(firstConfig), Object.keys(secondConfig));
   return configsKeys.map((key) => {
     const { type, process } = _.find(propertyActions, item => item.check(firstConfig, secondConfig, key));
-    const value = process(firstConfig[key], secondConfig[key], getAst);
+    const value = process(firstConfig[key], secondConfig[key], makeAst);
     return { name: key, type, value };
   });
 };
-// const makeAST = (data) => {
-//   const [first, ...rest] = data;
-//   const root = {
-//     name: first,
-//     value: '',
-//     children: [],
-//   };
-//   return rest.reduce((acc, arg) => {
-    
-//   }, root);
-// };
+const selection = (val, func, space) => {
+  if (val instanceof Array) return func(val, space + 2);
+  else if (val instanceof Object) return func([val], space + 2);
+  return val;
+};
+const render = (ast, space) => {
+  const tab = ' '.repeat(space);
+  const diff = ast.map((elem) => {
+    if (elem.type === 'nested') return `${tab}  ${elem.name}: ${selection(elem.value, render, space)}`;
+    else if (elem.type === 'inserted') return `${tab}+ ${elem.name}: ${selection(elem.value, render, space)}`;
+    else if (elem.type === 'not changed') return `${tab}  ${elem.name}: ${selection(elem.value, render, space)}`;
+    else if (elem.type === 'changed') return `${tab}- ${elem.name}: ${selection(elem.value.old, render, space)}\n${tab}+ ${elem.name}: ${selection(elem.value.new, render, space)}`;
+    else if (elem.type === 'deleted') return `${tab}- ${elem.name}: ${selection(elem.value, render, space)}`;
+    return `${tab}  ${Object.keys(elem)[0]}: ${elem[Object.keys(elem)[0]]}`;
+  }).join('\n');
+  return `{\n${diff}\n${' '.repeat(space - 2)}}`;
+};
 const gendiff = (path1, path2) => {
   const format1 = path.extname(path1);
   const format2 = path.extname(path2);
@@ -59,34 +65,8 @@ const gendiff = (path1, path2) => {
   const data2 = fs.readFileSync(path2, 'utf8');
   const obj1 = parse(format1)(data1);
   const obj2 = parse(format2)(data2);
-  console.log(makeAst(obj1, obj2));
-  const unionArrKey = _.union(Object.keys(obj1), Object.keys(obj2));
-  const diff = unionArrKey.map((elem) => {
-    if (_.has(obj2, elem) && _.has(obj1, elem)) {
-      if (obj1[elem] === obj2[elem]) return `    ${elem}: ${obj1[elem]}`;
-      return `  + ${elem}: ${obj2[elem]}  - ${elem}: ${obj1[elem]}`;
-    } else if (_.has(obj1, elem)) return `  - ${elem}: ${obj1[elem]}`;
-    return `  + ${elem}: ${obj2[elem]}`;
-  }).join('\n');
-  return `{\n${diff}}`;
+  const astDiff = makeAst(obj1, obj2);
+  return render(astDiff, 2);
 };
 export default gendiff;
 
-// const gendiff = (path1, path2) => {
-//   const format1 = path.extname(path1);
-//   const format2 = path.extname(path2);
-//   const data1 = fs.readFileSync(path1, 'utf8');
-//   const data2 = fs.readFileSync(path2, 'utf8');
-//   const obj1 = parse(format1)(data1);
-//   const obj2 = parse(format2)(data2);
-//   const unionArrKey = _.union(Object.keys(obj1), Object.keys(obj2));
-//   const diff = unionArrKey.map((elem) => {
-//     if (_.has(obj2, elem) && _.has(obj1, elem)) {
-//       if (obj1[elem] === obj2[elem]) return `    ${elem}: ${obj1[elem]}\n`;
-//       return `  + ${elem}: ${obj2[elem]}\n  - ${elem}: ${obj1[elem]}\n`;
-//     } else if (_.has(obj1, elem)) return `  - ${elem}: ${obj1[elem]}\n`;
-//     return `  + ${elem}: ${obj2[elem]}\n`;
-//   }).join('');
-//   return `{\n${diff}}`;
-// };
-// export default gendiff;
